8d479fa2b71f1d117bab117b0f809fcd322ee811
Egorov Anton <anton87egorov@gmail.com>
2024-01-07 22:52:52 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        Stream<Person> stream = persons.stream();
+        System.out.println(stream.filter(age -> age.getAge() < 18) .count());
+
+        Stream<Person> stream1 = persons.stream();
+        List<String> familySoldier = stream1.filter(sex -> sex.getSex() == Sex.MAN)
+        .filter(age -> age.getAge() > 17 && age.getAge() < 28)
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+        System.out.println(familySoldier.toString());
+//
+        Stream<Person> stream2 = persons.stream();
+        List<Person> peopleWorker = stream2.filter(education -> education.getEducation() == Education.HIGHER)
+                .filter(age -> age.getAge() > 18)
+                .filter(person -> person.getSex() == Sex.MAN && person.getAge() < 65 ||
+                        person.getSex() == Sex.WOMAN && person.getAge() < 60)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+        System.out.println(peopleWorker.toString());
+    }
+}
